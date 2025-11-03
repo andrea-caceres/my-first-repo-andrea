4fd050508cb35f9cb64707f983ede649793a5ab3
andrea-caceres <ac2376@georgetown.edu>
2025-11-03 11:29:14 -0500

    Update rps.py

--- app/rps.py
+++ app/rps.py
@@ -1,4 +1,5 @@
 import random
+<<<<<<< Updated upstream
 valid_choices = ["rock", "paper", "scissors"]
 user_choice = input("please choose an option ('rock', 'paper', or 'scissors'):").strip().lower()
 
@@ -25,3 +26,29 @@ else:
 
 print(result)
 print("Thanks for playing.")
+=======
+
+
+VALID_OPTIONS = ["rock", "paper", "scissors"]
+
+# ASK USER FOR AN INPUT (R/P/S)
+
+user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
+print("USER:", user_choice)
+
+# VALIDATIONS
+
+if user_choice not in VALID_OPTIONS:
+    print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
+    # exit()
+    quit()
+
+# GENERATE RANDOM COMPUTER CHOICE
+
+computer_choice = random.choice(VALID_OPTIONS)
+print("COMP:", computer_choice)
+
+# DETERMINE THE WINNER
+
+print("TODO:", "DETERMINE WINNER")
+>>>>>>> Stashed changes
